df745bbf83f4742bb38b0f6c9e2660f844fc5676
jfelipefg17 <jfelipefg17@gmail.com>
2023-07-15 17:40:00 -0500

    finish extra 3

--- java/POO/hangingGame/src/Main.java
+++ java/POO/hangingGame/src/Main.java
@@ -1,8 +1,28 @@
-
+import entities.Hangman;
+import services.HangmanServices;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Hangman g1 = new Hangman();
+        HangmanServices s1 = new HangmanServices();
+
+        g1 = s1.creatGame();
+
+        boolean ss1;
+
+            ss1 = s1.game(g1);
+            if (ss1){
+                System.out.println("/////////////////////////");
+                System.out.println("YOU WINNNNN !!!!!!!");
+                System.out.println("/////////////////////////");
+            }else{
+                System.out.println("/////////////////////////");
+                System.out.println("sorry you dont win");
+                System.out.println("/////////////////////////");
+            }
+
 
     }
 }
--- java/POO/hangingGame/src/services/HangmanServices.java
+++ java/POO/hangingGame/src/services/HangmanServices.java
@@ -10,30 +10,89 @@ public class HangmanServices {
     Scanner num = new Scanner(System.in);
 
     public Hangman creatGame () {
+
         System.out.println("write the word");
         String word = read.nextLine();
+        int n = word.length();
+
+        String [] array = new String[n];
+
+        for ( int i = 0 ; i < array.length; i ++ ) {
+            array[i] = word.substring(i,i+1);
+        }
+
         System.out.println("----------------");
         System.out.println("max trys");
         int max = num.nextInt();
+
+        return new Hangman(array,n,max);
     }
 
-    public void longWord () {
-
+    public void longWord (int longW) {
+        System.out.println("the long of the word is :" + longW);
     }
 
-    public boolean search (Hangman word) {
-        //ricibe the objet
+    public boolean search (String letter,Hangman word) {
+
+            boolean ss = true;
+            String [] array = word.getWord();
+            for ( int i = 0; i < word.getLongW(); i++ ) {
+                if (letter.equals(array[i])) {
+                    ss = true;
+                    break;
+                }else {
+                    ss = false;
+                }
+            }
+            return ss;
+        }
+
+
+
+    public boolean game (Hangman word) {
+
+        boolean asw = false;
+        int fe = word.getLongW();
+        int fo = 0;
+        int trys = word.getMax();
+
+        do {
+
+            System.out.println("-------------------");
+            System.out.println("write a letter");
+            String letter = read.nextLine();
+            System.out.println("-------------------");
+            System.out.println(letter);
+            System.out.println("-------------------");
+            longWord(word.getLongW());
+            System.out.println("-------------------");
+            boolean ss = search(letter, word);
+            if (ss) {
+                System.out.println("the letter IS in the word");
+                System.out.println("---------------------");
+                fo++;
+                fe--;
+                System.out.println("number of letter(founded / to found) are: (" + fo + " / " + fe + ")");
+                System.out.println("---------------------");
+                System.out.println("you have " + trys +" trys");
+            }else {
+                System.out.println("the letter IS NOT in the word");
+                System.out.println("---------------------");
+                System.out.println("number of letter(founded / to found) are: (" + fo + " / " + fe + ")");
+                System.out.println("---------------------");
+                trys = trys -1;
+                System.out.println("you have " + trys +" trys");
+
+            }
+
+            if(fo == word.getLongW() ) {
+                asw = true;
+                break;
+            }
+            System.out.println(" ");
+
+        }while(trys != 0);
+        return asw;
     }
 
-    public boolean found (String l) {
-
-    }
-
-    public void trys (Hangman word) {
-
-    }
-
-    public void game () {
-
-    }
 }
