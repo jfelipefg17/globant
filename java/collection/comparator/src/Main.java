151656bac18a9f0f0fda276daafa73b42f66a613
jfelipefg17 <jfelipefg17@gmail.com>
2023-07-25 21:34:23 -0500

    comparator

--- java/collection/comparator/src/Main.java
+++ java/collection/comparator/src/Main.java
@@ -9,7 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
         Dog d1 = new Dog("copper",10);
-        Dog d2 = new Dog("greta",3);
+        Dog d3 = new Dog("copperrrrr",11);
+        Dog d2 = new Dog("greta",90);
+        Dog d4 = new Dog("greta",2);
 
         ServiceDog s1 = new ServiceDog();
 
@@ -17,6 +19,8 @@ public class Main {
         ArrayList<Dog> arrayD = new ArrayList<>();
             arrayD.add(d1);
             arrayD.add(d2);
+            arrayD.add(d3);
+            arrayD.add(d4);
 
         for (Dog var : arrayD) {
             System.out.println(var);
@@ -24,18 +28,21 @@ public class Main {
 
         System.out.println("////////////////////////////");
 
-        arrayD.sort(ServiceDog.comp);
+        arrayD.sort(ServiceDog.compName);
+        //arrayD.sort(ServiceDog.compAge);
 
         for (Dog var : arrayD) {
             System.out.println(var);
         }
 
         System.out.println("/////////////////////////////");
-        
+
         // set
         HashSet<Dog> setD = new HashSet<>();
         setD.add(d1);
         setD.add(d2);
+        setD.add(d3);
+        setD.add(d4);
         ArrayList<Dog> listD = new ArrayList<>(setD);
 
         for (Dog var : listD) {
@@ -44,7 +51,7 @@ public class Main {
 
         System.out.println("/////////////////////////////");
 
-        listD.sort(ServiceDog.comp);
+        listD.sort(ServiceDog.compAge);
 
         for (Dog var : listD) {
             System.out.println(var);
@@ -65,6 +72,8 @@ public class Main {
         HashMap<Integer, Dog> map = new HashMap<>();
         map.put(1,d1);
         map.put(2,d2);
+        map.put(5,d3);
+        map.put(6,d4);
         ArrayList<Dog> dogs = new ArrayList<>(map.values());
 
         for (Dog var : dogs) {
@@ -73,7 +82,7 @@ public class Main {
 
         System.out.println("/////////////////////////////");
 
-        dogs.sort(ServiceDog.comp);
+        dogs.sort(ServiceDog.compAge);
 
         for (Dog var : dogs) {
             System.out.println(var);
--- java/collection/comparator/src/service/ServiceDog.java
+++ java/collection/comparator/src/service/ServiceDog.java
@@ -6,10 +6,18 @@ import java.util.Comparator;
 
 public class ServiceDog {
 
-    public static Comparator<Dog> comp = new Comparator<Dog>() {
+    public static Comparator<Dog> compAge = new Comparator<Dog>() {
         @Override
         public int compare(Dog d1, Dog d2) {
             return d2.getAge().compareTo(d1.getAge());
+        }
+
+    };
+
+    public static Comparator<Dog> compName = new Comparator<Dog>() {
+        @Override
+        public int compare(Dog d1, Dog d2) {
+            return d2.getName().compareTo(d1.getName());
         }
 
     };
