6779ca6ceb69b9346fbc08e082a09f2b0ae86581
jfelipefg17 <jfelipefg17@gmail.com>
2023-07-27 20:00:06 -0500

    example 5

---
+++ java/collection/example5/src/Main.java
@@ -0,0 +1,29 @@
+import service.ServiceCountry;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean anw;
+        Scanner boo = new Scanner(System.in);
+        ServiceCountry s1 = new ServiceCountry();
+
+        do {
+            s1.addCountry();
+            System.out.println("want add another country");
+            anw = boo.nextBoolean();
+        }while (anw);
+
+        s1.printCountry();
+        System.out.println("=============================");
+        s1.orderAZ();
+        System.out.println("=============================");
+        do {
+            s1.deleteCountry();
+            System.out.println("want delete another country");
+            anw = boo.nextBoolean();
+        }while (anw);
+        
+    }
+}
---
+++ java/collection/example5/src/service/ServiceCountry.java
@@ -0,0 +1,24 @@
+package service;
+
+public class ServiceCountry {
+
+    public void addCountry () {
+
+    }
+
+    public void printCountry () {
+        for (  ) {
+            System.out.println(" ");
+        }
+    }
+
+    public void orderAZ () {
+        //order
+        for (  ) {
+            System.out.println(" ");
+        }
+    }
+    public void deleteCountry () {
+
+    }
+}
