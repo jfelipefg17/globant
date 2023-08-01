48964e24df36952128140970ea387870832c1c82
jfelipefg17 <jfelipefg17@gmail.com>
2023-08-01 11:44:00 -0500

    finish example 6

--- java/collection/examplee6/src/Main.java
+++ java/collection/examplee6/src/Main.java
@@ -2,7 +2,7 @@ import service.ServiceProduct;
 
 import java.util.Scanner;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
 
--- java/collection/examplee6/src/service/ServiceProduct.java
+++ java/collection/examplee6/src/service/ServiceProduct.java
@@ -15,17 +15,33 @@ public class ServiceProduct {
         System.out.println("write the name of the product");
         String name = read.nextLine();
         System.out.println("write the price of the product");
-        Integer price = num.nextInt();
+        int price = num.nextInt();
+        listP.put(name,price);
 
     }
 
     public void modifyProduct () {
 
+        boolean ss = true;
         showProduct();
         System.out.println("//////////////////////////////////////////");
         System.out.println("write the name of the product you want modify");
         String name = read.nextLine();
 
+        for ( Map.Entry<String,Integer> entry : listP.entrySet()) {
+            if (entry.getKey().equals(name)) {
+                System.out.println("write the price of the product");
+                int price = num.nextInt();
+                entry.setValue(price);
+                ss = false;
+                break;
+            }
+        }
+        if (ss) {
+            System.out.println("THE PRODUCT IS NOT O0N THE LIST");
+        } else {
+            System.out.println("THE PRODUCT WAS MODIFY");
+        }
 
     }
 
@@ -35,7 +51,14 @@ public class ServiceProduct {
         System.out.println("//////////////////////////////////////////");
         System.out.println("write the name of the product you want delete");
         String name = read.nextLine();
-
+        int a1 = listP.size();
+        listP.remove(name);
+        int a2 = listP.size();
+            if (a1 == a2 ) {
+                System.out.println("THE PRODUCT IS NOT IN THE LIST");
+            } else {
+                System.out.println("THE PRODUCT WAS DELETE");
+            }
 
     }
 
