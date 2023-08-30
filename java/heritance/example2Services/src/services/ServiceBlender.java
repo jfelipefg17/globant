9c08983846a2a38c37a85d16199456791f92d7ec
jfelipefg17 <jfelipefg17@gmail.com>
2023-08-30 18:05:05 -0500

    some problems using services with inheritance, so make the example 2 in to ways 1 without service and one with service, this for learn better inheritance

--- java/heritance/example2Services/src/services/ServiceBlender.java
+++ java/heritance/example2Services/src/services/ServiceBlender.java
@@ -1,6 +1,6 @@
 package services;
 
 // todo ask if services extends super class or children class that is about the services
-// some one tell me that lavadora service extendes super class 
+// some one tell me that lavadora service extendes super class
 public class ServiceBlender {
 }
