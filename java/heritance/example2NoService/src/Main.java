fbd1e78db2f7d2638916e41cc36f3b3570db4478
jfelipefg17 <jfelipefg17@gmail.com>
2023-09-04 14:44:39 -0500

    finish example , start example 4

--- java/heritance/example2NoService/src/Main.java
+++ java/heritance/example2NoService/src/Main.java
@@ -1,22 +1,46 @@
+import entities.Appliance;
 import entities.Tv;
 import entities.WashingMachine;
+
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        WashingMachine w1 = new WashingMachine();
-        Tv tv1 = new Tv();
 
-        w1.createWashingMachine();
-        w1.finalPrice(w1);
 
-        tv1.createTv();
-        tv1.finalPrice(tv1);
 
-        System.out.println(w1.toString());
-        System.out.println("==========================");
-        System.out.println("==========================");
-        System.out.println(tv1.toString());
+        WashingMachine w1 = new WashingMachine(1000,"red","A",40,20);
+        WashingMachine w2 = new WashingMachine(1000,"blue","C",59,70);
+        Tv tv1 = new Tv(1000,"blue","D",30,27,true);
+        Tv tv2 = new Tv(1000,"grey","D",81,67,false);
+
+        ArrayList<Appliance> appliances = new ArrayList<>();
+
+        appliances.add(w1);
+        appliances.add(w2);
+        appliances.add(tv1);
+        appliances.add(tv2);
+
+        int j = 0;
+        for ( Appliance aux : appliances ) {
+            j ++;
+            System.out.println( "Appliance " + j + ": " + aux.getPrice() );
+            aux.finalPrice(aux);
+        }
+
+        double totalAdd = 0;
+        int i = 0;
+        for ( Appliance aux : appliances ) {
+            i ++;
+            System.out.println( "Appliance " + i + ": " + aux.getPrice() );
+            totalAdd = totalAdd + aux.getPrice();
+        }
+
+        System.out.println("===================");
+        System.out.println("TOTAL ADD: " + totalAdd);
+        System.out.println("===================");
+
 
     }
 }
