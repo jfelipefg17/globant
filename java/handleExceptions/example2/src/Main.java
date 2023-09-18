ec2a69d068517075715fb082bfea11bdfadec175
jfelipefg17 <jfelipefg17@gmail.com>
2023-09-18 17:59:57 -0500

    finish example 1,2,3

--- java/handleExceptions/example2/src/Main.java
+++ java/handleExceptions/example2/src/Main.java
@@ -1,17 +1,26 @@
+import java.lang.reflect.Array;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for ( int i = 1; i <= 5; i++ ) {
+        int [] array = new int [5];
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            for ( int i = 1; i <= 10; i++ ) {
+
+                array[i] = i;
+
+            }
+        }catch (ArrayIndexOutOfBoundsException a) {
+            System.out.println("sorry array out of bounds");
+            System.out.println("the lenght of the array is: " + array.length);
+            System.out.println(a.getMessage());
+        }catch (Exception a){
+            System.out.println(a.getMessage());
         }
+
+
     }
 }
--- java/handleExceptions/example3/src/Main.java
+++ java/handleExceptions/example3/src/Main.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for ( int i = 1; i <= 5; i++ ) {
+        Scanner num = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+        System.out.println("enter a number");
+        String n1 = num.nextLine();
+        System.out.println("enter a number ");
+        String n2 = num.nextLine();
+
+
+
+        try {
+            int n3 = parseInt(n1);
+            int n4 = parseInt(n2);
+            int div = n3 / n4;
+            System.out.println("the division is: " + div);
+        }catch (NumberFormatException a) {
+            // this is for see all de collapsed mistakes a.printStackTrace();
+            System.out.println("the string cant be converted to a int ");
+            System.out.println(a.getMessage());
+        }catch (Exception a) {
+            a.printStackTrace();
+            System.out.println(a.getMessage());
         }
     }
 }
