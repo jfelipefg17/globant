ae99877b128ff7c45da340a450d9a89f216210a1
jfelipefg17 <jfelipefg17@gmail.com>
2023-09-28 20:59:55 -0500

    finish mysql documents, iu will start examples

--- first-MySQL/first.sql
+++ first-MySQL/first.sql
@@ -10,12 +10,21 @@ CREATE TABLE `first_data_base`.`students` (
   `name` VARCHAR(255) NULL,
   `age` INT NULL,
   PRIMARY KEY (`id_student`));
-
+  
+CREATE TABLE `first_data_base`.`students2` (
+  `id_student` INT NOT NULL,
+  `name` VARCHAR(255) NULL,
+  `age` INT NULL,
+  PRIMARY KEY (`id_student`));
 /*INSERT DATA IN THE TABLE*/
 
 INSERT INTO students VALUES(1,'felipe',30);
-INSERT INTO students VALUES(2,'oscar',20);
-INSERT INTO students VALUES(3,'andres',56);
+INSERT INTO students VALUES(2,'maria',20);
+INSERT INTO students VALUES(3,'andres',50);
+
+INSERT INTO students2 VALUES(1,'felipe',30);
+INSERT INTO students2 VALUES(2,'oscar',20);
+INSERT INTO students2 VALUES(3,'andres',56);
 
 /*SELECT EVERYTHING OF THE TABLE*/
 SELECT *
@@ -30,6 +39,10 @@ WHERE id_student = 1;
 DELETE 
 	FROM students
 WHERE id_student = 3;
+
+/*DELETE OR DROP TABLE/SCHEMA ETC*/
+DROP TABLE students;
+DROP SCHEMA first_data_base;    
 
 /*SELECT IF CONDITION*/
 SELECT NAME FROM STUDENTS;
@@ -49,16 +62,32 @@ SELECT NAME, SUM(AGE) FROM STUDENTS;
 SELECT COUNT(ID_STUDENT), NAME FROM STUDENTS;
 
 /*HAVING*/
+/*agruparia cuantas personas hay por pais y que solo las muestre si ese pais tiene mas de una persona*/
+SELECT COUNT(ID), pais FROM Personas GROUP BY pais HAVING COUNT(ID) > 1;
+
+/*AS*/
+/*PUT A NICK NAME TO A COLOMUN THAT WE SAID*/
+SELECT NAME AS NAMESTUDENT FROM STUDENTS;
 
 
+/*ROUND*/
+SELECT ROUND(AVG(AGE)) FROM STUDENTS;
+
+/*LIMIT*/
+SELECT NAME FROM STUDENTS LIMIT 1;
 
 
+/*JOIN*/
 
+/*INNER JOIN*/
+SELECT * FROM  students INNER JOIN students2 ON students.id_student = students2.id_student;
 
+/*LEFT JOIN*/
+SELECT * FROM  students LEFT JOIN students2 ON students.id_student = students2.id_student;
 
+/*RIGHT JOIN*/
+SELECT * FROM  students RIGHT JOIN students2 ON students.id_student = students2.id_student;
 
-
-
-/*DELETE OR DROP TABLE/SCHEMA ETC*/
-DROP TABLE student;
-DROP SCHEMA first_data_base;    
+/*SUB QUERY*/
+/*IT IS FOR USE A QUERY INSIDE OF AND OTHER QUERY*/
+SELECT nombre, salario FROM Empleados WHERE salario > (SELECT AVG(salario) FROM Empleados);
