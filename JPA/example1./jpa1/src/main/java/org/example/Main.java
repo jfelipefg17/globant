d203fb826b1b96034698acdb3e3a773acf4780f9
jfelipefg17 <jfelipefg17@gmail.com>
2023-10-24 20:17:38 -0500

    setting everything to work with JPA

---
+++ JPA/example1./jpa1/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    EntityManager em = emf.createEntityManager();
+  }
+
+}
