79e515eeead26d4e66cd61b110ddd0206929e3e2
jfelipefg17 <jfelipefg17@gmail.com>
2023-10-26 19:25:55 -0500

    still working

--- JPA/example2/libreria/src/main/libreria/libreria.services/AuthorService.java
+++ JPA/example2/libreria/src/main/libreria/libreria.services/AuthorService.java
@@ -1,4 +1,11 @@
 package libreria.services;
 
+import libreria.entities.Author;
+
 public class AuthorService {
+
+  public Author searchAuthorByName() {
+    Author author = new Author();
+    return author;
+  }
 }
--- JPA/example2/libreria/src/main/libreria/libreria.services/BookService.java
+++ JPA/example2/libreria/src/main/libreria/libreria.services/BookService.java
@@ -1,4 +1,27 @@
 package libreria.services;
 
+import libreria.entities.Book;
+
 public class BookService {
+
+
+  public Book searchBookByIsbn(){
+    Book book = new Book();
+    return book;
+  }
+
+  public Book searchBookByTittle(){
+    Book book = new Book();
+    return book;
+  }
+
+  public Book searchBookByNameAuthor(){
+    Book book = new Book();
+    return book;
+  }
+
+  public Book searchBookByNamePublisher(){
+    Book book = new Book();
+    return book;
+  }
 }
